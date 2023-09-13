96650531f64719a43052fcf8e0d8f8deff1bfb35
borel-T <borel-T@users.noreply.github.com>
2023-09-13 04:23:26 +0300

    added unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import LandingPage from "./containers/auth/landing";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders landing page", async () => {
+  render(<LandingPage />, { wrapper: BrowserRouter });
+  // check we are on landing page
+  expect(screen.getByText(/Ejara Coding Challenge/i)).toBeInTheDocument();
+  // check Note-app card renders on landing pagte
+  expect(screen.getByText(/Google Keep App/i)).toBeInTheDocument();
 });
